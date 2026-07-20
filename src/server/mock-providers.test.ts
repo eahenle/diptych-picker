dfe75dd21dfafb38816d1cf2c8812d9a7c604a18
Adrian Henle <ahenle@cisco.com>
2026-07-20 09:06:30 -0700

    Stabilize mock image provider test

--- src/server/mock-providers.test.ts
+++ src/server/mock-providers.test.ts
@@ -57,5 +57,5 @@ describe("mock providers", () => {
       Buffer.from([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]),
     );
     expect(repeated.bytes).toEqual(image.bytes);
-  });
+  }, 15_000);
 });
